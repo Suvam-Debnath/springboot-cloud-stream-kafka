7e3fe38f8cfb71e658e123206e2a3f3919845107
Suvam Debnath <debnathsuvam322@gmail.com>
2025-12-11 22:38:37 +0530

    added consumer with spring cloud stream

---
+++ consumer/src/main/java/com/suvam/consumer/RiderLocation.java
@@ -0,0 +1,15 @@
+package com.suvam.consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RiderLocation {
+    private String riderId;
+    private double latitude;
+    private double longitude;
+
+}
---
+++ consumer/src/test/java/com/suvam/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suvam.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
